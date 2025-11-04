8d6e3a733f3632d5b4c31c13fa7ccd9334089b61
nicholaspsmith <npsmith1990@gmail.com>
2025-11-04 15:12:15 -0500

    Add resume page; remove link to work page

--- frontend/src/app/resume/resume.ts
+++ frontend/src/app/resume/resume.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './resume.css',
 })
 export class Resume {
-
+  downloadResume(): void {
+    const link = document.createElement('a');
+    link.href = '/resume/Nicholas_Smith_Resume_2025.pdf';
+    link.download = 'Nicholas_Smith_Resume_2025.pdf';
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
