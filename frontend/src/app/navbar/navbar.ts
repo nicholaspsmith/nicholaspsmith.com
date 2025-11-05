cc7626c7979b5e180fc6ca2b9b4fbb172a5d4ed8
nicholaspsmith <npsmith1990@gmail.com>
2025-11-04 22:42:06 -0500

    Update mobile styles for home.html; Add mobile nav functionality

--- frontend/src/app/navbar/navbar.ts
+++ frontend/src/app/navbar/navbar.ts
@@ -1,11 +1,31 @@
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  menuClass = 'max-h-0';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: { constructor: { name: string; }; }) => {
+      if (event.constructor.name === 'NavigationStart') {
+        this.closeMenu();
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.menuClass = this.menuClass === 'max-h-0' ? 'max-h-48' : 'max-h-0';
+  }
+
+  closeMenu() {
+    this.menuClass = 'max-h-0';
+  }
 }
