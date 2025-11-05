4af181fbca1f370676cff82a8ca9e6fcb9d1e5d2
nicholaspsmith <npsmith1990@gmail.com>
2025-11-05 13:28:34 -0500

    Switch to hash routes to support gh-pages

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -7,7 +7,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withViewTransitions())
+    provideRouter(routes, withViewTransitions(), withHashLocation())
   ]
 };
 
